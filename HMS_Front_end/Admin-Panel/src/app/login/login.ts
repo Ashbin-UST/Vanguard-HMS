718d76b0357a03fcc62c5151896cc7869899f469
Ashbin-UST <308321@ust.com>
2026-05-16 09:23:23 +0530

    Add signup and login page

--- HMS_Front_end/Admin-Panel/src/app/login/login.ts
+++ HMS_Front_end/Admin-Panel/src/app/login/login.ts
@@ -1,9 +1,30 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {}
+
+export class Login {
+  loginForm: FormGroup;
+
+  constructor(private readonly fb: FormBuilder) {
+
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]]
+    });
+
+  }
+
+  onSubmit() {
+
+    if (this.loginForm.valid) {
+      console.log(this.loginForm.value);
+    }
+  }
+}
