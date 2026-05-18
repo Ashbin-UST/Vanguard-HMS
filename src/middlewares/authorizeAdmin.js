c795d134d421a6c74b48aeb4c5c89a7510a122c6
Ezbon Puthanangady Joy(UST <308327@ust.com>
2026-05-18 16:15:19 +0530

    folder restructure

--- src/middlewares/authorizeAdmin.js
+++
@@ -1,8 +0,0 @@
-const authorizeAdmin = (req,res,next)=>{
-    if(req.user.role!=='ADMIN'){
-        return res.status(409).json({"message":"Access Denied"})
-    }
-    next();
-};
-
-module.exports=authorizeAdmin;
