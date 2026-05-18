4894d93ae35dacadc1badd47c7e7f377f669c900
Ashbin003 <ashbinachu003@gmail.com>
2026-05-18 23:52:19 +0530

    Implement multi-step signup flow with dynamic validation

--- HMS_Front_end/Admin-Panel/src/app/app.routes.ts
+++ HMS_Front_end/Admin-Panel/src/app/app.routes.ts
@@ -1,13 +1,11 @@
 import { Routes } from '@angular/router';
-import {Login} from "./login/login";
-import { Signup } from './signup/signup'; 
+import { Login } from './login/login';
+import { Signup } from './signup/signup';
 import { User } from './user/user';
 import { Dashboard } from './dashboard/dashboard';
 
 export const routes: Routes = [
-    {path:'signup',component:Signup},
-    {path:'',component:Login},
-    {path:'dashboard',component:Dashboard},
-    
-
+  { path: 'signup', component: Signup },
+  { path: '', component: Login },
+  { path: 'dashboard', component: Dashboard },
 ];
