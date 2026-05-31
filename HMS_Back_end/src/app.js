9b6632356b4a953a660915fb1bfb72bde8faeda4
Ashbin003 <ashbinachu003@gmail.com>
2026-05-31 15:35:19 +0530

    Edit db connectivity

--- HMS_Back_end/src/app.js
+++ HMS_Back_end/src/app.js
@@ -4,7 +4,6 @@ const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
 const morgan = require("morgan");
-const mongoose = require("mongoose");
 
 const authRoutes = require("./routes/authRoutes");
 const adminRoutes = require("./routes/adminRoutes");
@@ -21,10 +20,10 @@ app.use(helmet());
 
 // Enable CORS
 app.use(
-    cors({
-        origin: process.env.FRONTEND_URL,
-        credentials: true
-    })
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  })
 );
 
 // Middleware which logs requests
@@ -43,32 +42,10 @@ app.use("/api/employees", employeeRoutes);
 app.use("/api/nodes", nodeRoutes);
 
 // Default route
-app.get("/", (req, res) => res.json({
-    message: "API running"
-}));
-
-// MongoDB connection
-const connectDB = async () => {
-
-    try {
-
-        await mongoose.connect(
-            process.env.MONGO_URI
-        );
-
-        console.log("MongoDB connected");
-
-    } catch (err) {
-
-        console.error(
-            "MongoDB connection error:",
-            err.message
-        );
-
-        process.exit(1);
-    }
-};
-
-connectDB();
+app.get("/", (req, res) =>
+  res.json({
+    message: "API running",
+  })
+);
 
 module.exports = app;
